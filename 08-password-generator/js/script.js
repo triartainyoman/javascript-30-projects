3d6d22be87b985b060605dcd14b2da744125b24a
Triarta <triarta.art@gmail.com>
2023-07-16 21:29:12 +0800

    08: Finish generate random password logic

---
+++ 08-password-generator/js/script.js
@@ -0,0 +1,21 @@
+const generatePassword = () => {
+  const passwordLength = document.getElementById("password-length").value;
+  const randomCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+  let password = "";
+
+  for (let i = 0; i < passwordLength; i++) {
+    const random = Math.floor(Math.random() * randomCharacters.length);
+    password += randomCharacters[random];
+  }
+
+  document.getElementById("result-password").value = password;
+};
+
+const handleCopy = () => {
+  let copyText = document.getElementById("result-password");
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+};
