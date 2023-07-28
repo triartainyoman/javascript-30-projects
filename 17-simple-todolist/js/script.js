7e1596855ab1be33a3079138dea6bca70bcc58d2
Triarta <triarta.art@gmail.com>
2023-07-28 23:17:45 +0800

    17: Implement clean code

--- 17-simple-todolist/js/script.js
+++ 17-simple-todolist/js/script.js
@@ -2,26 +2,41 @@ const inputTodo = document.getElementById("input-todo");
 const todos = document.getElementById("todos");
 
 const addTodo = () => {
-  const todoText = inputTodo.value;
-  if (todoText == "") {
-    return false;
+  const todoText = inputTodo.value.trim();
+  if (!todoText) {
+    return;
   }
 
-  const todoItem = document.createElement("li");
-  todoItem.classList.add("todo");
-  todoItem.textContent = todoText;
-
-  const deleteButton = document.createElement("button");
-  deleteButton.onclick = () => {
-    todos.removeChild(todoItem);
-  };
-
-  const icon = document.createElement("img");
-  icon.src = "/img/delete.svg";
-
-  deleteButton.appendChild(icon);
-  todoItem.appendChild(deleteButton);
+  const todoItem = createTodoElement(todoText);
   todos.appendChild(todoItem);
 
   inputTodo.value = "";
 };
+
+const createTodoElement = (todoText) => {
+  const todoItem = document.createElement("li");
+  todoItem.classList.add("todo");
+  todoItem.textContent = todoText;
+
+  const deleteButton = createDeleteButton();
+  deleteButton.onclick = () => {
+    todos.removeChild(todoItem);
+  };
+
+  todoItem.appendChild(deleteButton);
+  return todoItem;
+};
+
+const createDeleteButton = () => {
+  const deleteButton = document.createElement("button");
+  const icon = document.createElement("img");
+  icon.src = "/img/delete.svg";
+  deleteButton.appendChild(icon);
+  return deleteButton;
+};
+
+inputTodo.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    addTodo();
+  }
+});
