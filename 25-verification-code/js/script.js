1dcd4fdfaefb93a2f1043d342eb33fa3d41e9576
Triarta <triarta.art@gmail.com>
2023-10-02 23:23:01 +0800

    25: Edit

--- 25-verification-code/js/script.js
+++ 25-verification-code/js/script.js
@@ -7,10 +7,6 @@ digitInputs.forEach((input, index) => {
     if (inputValue.length === 1) {
       if (index < digitInputs.length - 1) {
         digitInputs[index + 1].focus();
-      } else {
-        const enteredCode = Array.from(digitInputs)
-          .map((input) => input.value)
-          .join("");
       }
     } else if (inputValue === "" && index > 0) {
       digitInputs[index - 1].focus();
